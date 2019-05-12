264e710ced36be04536935e3e9bd9975f2299e78
etsvet <ecvetanov@gmail.com>
2019-05-13 01:03:25 +0300

    WIP

---
+++ server/start.py
@@ -0,0 +1,21 @@
+import falcon
+from pymongo import MongoClient
+from pprint import pprint
+
+# settings API
+# simulation START/STOP
+# COMBINE ^ ?
+# iteration
+
+client = MongoClient('mongodb://127.0.0.1:27017')ds
+
+class IterationsResource:
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = "THIS WORKS YES"
+        db = client.test
+        db.zips.find()
+
+app = falcon.API()
+iterations = IterationsResource()
+app.add_route('/iterations', iterations)
