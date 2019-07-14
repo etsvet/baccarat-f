5850d17dc0dbe05fd3d0b96daaedaed7300d790f
etsvet <ecvetanov@gmail.com>
2019-07-14 22:16:29 +0300

    Add Polymer library

---
+++ client/src/_element/_element.js
@@ -0,0 +1,28 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+class BaccaratClientApp extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'baccarat-client-app'
+      }
+    };
+  }
+}
+
+window.customElements.define('baccarat-client-app', BaccaratClientApp);
